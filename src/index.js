b6befaadce2719fbe08cc7bdde507e7737d0b0d0
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-04-02 12:13:10 +0300

    refactor pagination

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import { removeGlobalLoader } from './js/services/loader';
 import { showTrailer } from './js/services/trailer';
 import getGenres from './js/utiles/get_geners';
 
-console.log(STATE);
-async function get() {
+// console.log(STATE);
+async function get(page) {
   //   const date = await moviesAPI.getMovieByKeyword('cat', 1);
   //   const date = await moviesAPI.getMovieDetails(76600);
   //   const date = await moviesAPI.getRelatedVideos(76600);
-  // const date = await moviesAPI.getPopMovies(3);
+  const date = await moviesAPI.getPopMovies(page);
+  stateModify(date);
+  startPage(STATE.totalPages, moviesAPI.getPopMovies);
   // console.log(date);
   // Remove comment to check trailer modal
   // const firestFilmId = date.results[0].id;
   // showTrailer(firestFilmId);
 }
-// get();
-// getGenres();
+get(STATE.page);
+
 // Remove global loader when page loaded
 window.addEventListener('load', removeGlobalLoader);
+
+function stateModify(date) {
+  STATE.movies = date.results;
+  STATE.totalPages = date.total_pages;
+  console.log('STATE', STATE);
+}
