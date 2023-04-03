1d116102c90a998b3fb476e07e71402c3c74f4ee
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-04-03 11:38:45 +0300

    Anja modal-film.js

--- src/js/services/modal-film.js
+++ src/js/services/modal-film.js
@@ -19,18 +19,11 @@ export const refs = {
   btnAddQueueEl: '',
   btnRemoveWatchedEl: '',
   btnRemoveQueueEl: '',
-  btnViewTrailer: '',
 };
 
 // Const
 let filmId = null;
-const DATA = [
-  'add-watched',
-  'add-queue',
-  'remove-watched',
-  'remove-queue',
-  'trailer',
-];
+const DATA = ['add-watched', 'add-queue', 'remove-watched', 'remove-queue'];
 const KEY_LIBRIARY = ['watched', 'queue'];
 const IS_HIDDEN = 'visually-hidden';
 
@@ -42,12 +35,11 @@ export async function createFilmModal(e) {
   // Create Backdrop
   refs.bodyEl.insertAdjacentHTML('beforeend', renderBackdrop());
   refs.filmBackdropEl = document.querySelector('.backdrop');
-  // Create Modal
+
   try {
+    // Fetch MovieDetails
     const data = await moviesAPI.getMovieDetails(filmId);
-
     // Create Modal
-
     refs.filmBackdropEl.insertAdjacentHTML('afterbegin', renderModal(data));
   } catch (error) {
     console.log(error);
@@ -56,17 +48,6 @@ export async function createFilmModal(e) {
   btnElSelect();
   // Show/Hide Buttons
   btnElShowHide();
-
-  // Create YouTubePlayer
-  try {
-    const videos = await moviesAPI.getRelatedVideos(filmId);
-    trailerId = getTrailerId(videos);
-    createYouTubePlayer(trailerId);
-  } catch (error) {
-    refs.btnViewTrailer.textContent = error;
-    refs.btnViewTrailer.disabled = true;
-  }
-
   // Add Modal Listners
   refs.filmModalCloseBtnlEl.addEventListener('click', closeFilmModal);
   refs.filmBackdropEl.addEventListener('click', closeFilmModal);
@@ -76,13 +57,13 @@ export async function createFilmModal(e) {
 
 // Select Button Elements
 function btnElSelect() {
-  refs.filmModalEl = document.querySelector('[data-modal="modal"]');
   refs.filmModalCloseBtnlEl = document.querySelector('.modal__close');
+  refs.filmModalEl = document.querySelector('[data-modal="modal"]');
+
   refs.btnAddWatchedEl = document.querySelector(`[data-modal=${DATA[0]}]`);
   refs.btnAddQueueEl = document.querySelector(`[data-modal=${DATA[1]}]`);
   refs.btnRemoveWatchedEl = document.querySelector(`[data-modal=${DATA[2]}]`);
   refs.btnRemoveQueueEl = document.querySelector(`[data-modal=${DATA[3]}]`);
-  refs.btnViewTrailer = document.querySelector(`[data-modal=${DATA[4]}]`);
 }
 
 // Show/Hide Buttons
@@ -131,16 +112,7 @@ function onBtnClick(e) {
       refs.btnAddQueueEl.classList.toggle(IS_HIDDEN);
       break;
     case 'trailer':
-
-      youTubePlayer.playVideo();
-      refs.youTubePlayerEl = refs.filmBackdropEl.querySelector('iframe');
-      refs.youTubePlayerEl.classList.toggle('visually-hidden');
-      refs.filmModalEl.classList.toggle('visually-hidden');
-      e.target.closest('.btn__trailer').blur();
-      youTubePlayer.on('stateChange', e => refs.youTubePlayerEl.blur());
-
-      //showTrailer(filmId);
-
+      showTrailer(filmId);
       break;
   }
 }
@@ -158,36 +130,7 @@ function closeFilmModal(e) {
     refs.filmBackdropEl.removeEventListener('click', closeFilmModal);
     removeEventListener('keydown', closeFilmModal);
 
-    removeEventListener('keydown', hideYouTubePlayer);
-
-
-
     // Add FilmCardGallery Listner
     refs.filmCardListEl.addEventListener('click', createFilmModal);
   }
 }
-
-
-// // Hide YouTubePlayer
-function hideYouTubePlayer(e) {
-  if (e.code === 'Escape' || e.target.className === 'backdrop') {
-    refs.youTubePlayerEl.classList.add('visually-hidden');
-    refs.filmModalEl.classList.toggle('visually-hidden');
-    youTubePlayer.stopVideo();
-  }
-}
-
-// TrailerId
-function getTrailerId(videos) {
-  const officialTrailer = videos.results.find(el =>
-    el.name.toLowerCase().includes('official trailer')
-  );
-  if (officialTrailer) return officialTrailer.key;
-  const trailer = videos.results.find(el =>
-    el.name.toLowerCase().includes('trailer')
-  );
-  if (trailer) return trailer.key;
-  if (videos.length) return videos[0].key;
-  throw new Error('Oops! Trailer not found');
-}
-
