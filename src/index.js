9611b9c1305c484be26255acbd4478d267f18069
AndreyKonash <konash.andrey@gmail.com>
2023-04-03 17:50:41 +0300

    add firebase

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import { startPage } from './js/components/pagination';
 import { removeGlobalLoader } from './js/services/loader';
 import { showTrailer } from './js/services/trailer';
 import { refs, createFilmModal } from './js/services/modal-film';
-
+import {
+  app,
+  signUpWithGoogle,
+  signOutFromGoogle,
+} from './js/services/firebaseAPI.js';
 // Remove global loader when page loaded
 window.addEventListener('load', removeGlobalLoader);
 get(STATE.page);
@@ -26,3 +30,14 @@ function stateModify(date) {
   STATE.movies = date.results;
   STATE.totalResults = date.total_results;
 }
+
+const googleSignUp = document.querySelector('.registration-menu-singBtn');
+const googleSignOut = document.querySelector('.registration-menu-logBtn');
+const myLibrary = document.querySelector('.js-my-library-btn');
+
+googleSignUp.addEventListener('click', () => {
+  signUpWithGoogle(app);
+});
+googleSignOut.addEventListener('click', () => {
+  signOutFromGoogle(app);
+});
