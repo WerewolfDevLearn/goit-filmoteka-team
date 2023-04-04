706d9eddd311f3308b3025da3f5d71a837dfd551
Malberee <82259650+Malberee@users.noreply.github.com>
2023-04-04 23:48:47 +0300

    fix icons in progress

--- src/js/services/login.js
+++ src/js/services/login.js
@@ -36,46 +36,44 @@ function loginChoose(e) {
   }
 }
 
-  function authModalEvents(e) {
-    if (!e.target.closest('.auth__wrap') || e.target.closest('.close-btn'))
-      closeAuthModal();
+function authModalEvents(e) {
+  if (!e.target.closest('.auth__wrap') || e.target.closest('.close-btn'))
+    closeAuthModal();
 
-    if (e.target.closest('.show-password-icon')) toggleShowPassword();
+  if (e.target.closest('.show-password-icon')) toggleShowPassword();
 
-    if (e.target.closest('#linkSigninAccount')) showLoginForm();
-    else if (e.target.closest('#linkCreateAccount')) showSignupForm();
+  if (e.target.closest('#linkSigninAccount')) showLoginForm();
+  else if (e.target.closest('#linkCreateAccount')) showSignupForm();
 
-    if (e.target.closest('.login-with-google-btn')) signinWithGoogle();
-  }
+  if (e.target.closest('.login-with-google-btn')) signinWithGoogle();
+}
 
-  function toggleShowPassword() {
-    const showIcon = document.querySelector('.show-password-icon');
-    const inputPassword = document.querySelector(
-      '.form__input[name="password"]'
+function toggleShowPassword() {
+  const showIcon = document.querySelector('.show-password-icon');
+  const inputPassword = document.querySelector('.form__input[name="password"]');
+  const type =
+    inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+  inputPassword.setAttribute('type', type);
+  if (type === 'password') {
+    showIcon.firstElementChild.setAttribute(
+      'href',
+      './images/icons.svg#icon-not-show-password'
     );
-    const type =
-      inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-    inputPassword.setAttribute('type', type);
-    if (type === 'password') {
-      showIcon.firstElementChild.setAttribute(
-        'href',
-        './images/icons.svg#icon-not-show-password'
-      );
-    } else if (type === 'text') {
-      showIcon.firstElementChild.setAttribute(
-        'href',
-        './images/icons.svg#icon-show-password'
-      );
-    }
+  } else if (type === 'text') {
+    showIcon.firstElementChild.setAttribute(
+      'href',
+      './images/icons.svg#icon-show-password'
+    );
   }
+}
 
-  function closeAuthModal() {
-    backdrop.remove()
-    backdrop.removeEventListener('click', authModalEvents);
-  }
+function closeAuthModal() {
+  backdrop.remove();
+  backdrop.removeEventListener('click', authModalEvents);
+}
 
-  function showSignupForm() {
-    backdrop.firstElementChild.innerHTML = `<button class="close-btn">
+function showSignupForm() {
+  backdrop.firstElementChild.innerHTML = `<button class="close-btn">
 	<img src="./images/close.svg" alt="" />
 </button>
 <form class="auth__form" id="signup">
@@ -89,7 +87,7 @@ function loginChoose(e) {
 			placeholder="E-mail"
 		/>
 		<svg class="email-icon">
-							<use href="./images/icons.svg#icon-email"></use>
+							<use href="./images/icons.svg#icon-email-form"></use>
 						</svg>
 	</div>
 	<div class="form__input-group">
@@ -119,12 +117,12 @@ function loginChoose(e) {
 		>
 	</p>
 	</form>`;
-    signupForm = document.getElementById('signup');
-    signupForm.addEventListener('submit', onSignupSubmit);
-  }
+  signupForm = document.getElementById('signup');
+  signupForm.addEventListener('submit', onSignupSubmit);
+}
 
-  function showLoginForm() {
-    backdrop.firstElementChild.innerHTML = `
+function showLoginForm() {
+  backdrop.firstElementChild.innerHTML = `
 	<button class="close-btn">
 					<img src="./images/close.svg" alt="" />
 				</button>
@@ -168,101 +166,99 @@ function loginChoose(e) {
 						>
 					</p>
 				</form>`;
-    loginForm = document.getElementById('login');
-    loginForm.addEventListener('submit', onLoginSubmit);
-  }
+  loginForm = document.getElementById('login');
+  loginForm.addEventListener('submit', onLoginSubmit);
+}
 
-  function onLoginSubmit(e) {
-    e.preventDefault();
+function onLoginSubmit(e) {
+  e.preventDefault();
 
-    loginForm = document.getElementById('login');
-    loginMsgError = document.querySelector('.login-form__message-error');
-    const { email, password } = loginForm.elements;
+  loginForm = document.getElementById('login');
+  loginMsgError = document.querySelector('.login-form__message-error');
+  const { email, password } = loginForm.elements;
 
-    console.log(email.value, password.value.trim());
+  console.log(email.value, password.value.trim());
 
-    // signInWithEmailAndPassword(auth, email.value, password.value.trim())
-    sinInWithEmailPassword()
-      .then(() => {
-        alert(`You are logged in.`);
-        closeAuthModal();
-        loginMsgError.textContent = '';
-        loginForm.reset();
-        signupForm.reset();
-      })
-      .catch(err => {
-        if (
-          err.code === 'auth/wrong-password' ||
-          err.code === 'auth/user-not-found'
-        ) {
-          loginMsgError.textContent = 'Incorrect email or password';
-        }
-        // console.log(err)
-      });
-  }
+  // signInWithEmailAndPassword(auth, email.value, password.value.trim())
+  sinInWithEmailPassword()
+    .then(() => {
+      alert(`You are logged in.`);
+      closeAuthModal();
+      loginMsgError.textContent = '';
+      loginForm.reset();
+      signupForm.reset();
+    })
+    .catch(err => {
+      if (
+        err.code === 'auth/wrong-password' ||
+        err.code === 'auth/user-not-found'
+      ) {
+        loginMsgError.textContent = 'Incorrect email or password';
+      }
+      // console.log(err)
+    });
+}
 
-  function onSignupSubmit(e) {
-    e.preventDefault();
+function onSignupSubmit(e) {
+  e.preventDefault();
 
-    signupForm = document.getElementById('signup');
-    signupMsgError = document.querySelector('.signup-form__message-error');
-    const { email, password } = signupForm.elements;
-    console.log(email.value, password.value.trim());
+  signupForm = document.getElementById('signup');
+  signupMsgError = document.querySelector('.signup-form__message-error');
+  const { email, password } = signupForm.elements;
+  console.log(email.value, password.value.trim());
 
-    userCreation(email.value, password.value)
-      .then(() => {
-        alert(`User created!`);
-        closeAuthModal();
-        signupMsgError.textContent = '';
-      })
-      .catch(err => {
-        if (err.code === 'auth/email-already-in-use')
-          signupMsgError.textContent = 'Email Already in use';
-        if (err.code === 'auth/weak-password') {
-          signupMsgError.textContent =
-            'Password should be at least 6 characters';
-        }
-        // console.log(err)
-      });
-  }
+  userCreation(email.value, password.value)
+    .then(() => {
+      alert(`User created!`);
+      closeAuthModal();
+      signupMsgError.textContent = '';
+    })
+    .catch(err => {
+      if (err.code === 'auth/email-already-in-use')
+        signupMsgError.textContent = 'Email Already in use';
+      if (err.code === 'auth/weak-password') {
+        signupMsgError.textContent = 'Password should be at least 6 characters';
+      }
+      // console.log(err)
+    });
+}
 
-  function signinWithGoogle() {
-    // signInWithPopup(auth, provider)
-    signInWithGoogle()
-      .then(result => {
-        alert(`You are signed in with Google`);
-        closeAuthModal();
-      })
-      .catch(err => {
-        if (
-          err.code === 'auth/popup-closed-by-user' ||
-          err.code === 'auth/cancelled-popup-request'
-        ) {
-          return;
-        }
-        alert(err.code);
-        console.log(err);
-      });
-  }
+function signinWithGoogle() {
+  // signInWithPopup(auth, provider)
+  signInWithGoogle()
+    .then(result => {
+      alert(`You are signed in with Google`);
+      closeAuthModal();
+    })
+    .catch(err => {
+      if (
+        err.code === 'auth/popup-closed-by-user' ||
+        err.code === 'auth/cancelled-popup-request'
+      ) {
+        return;
+      }
+      alert(err.code);
+      console.log(err);
+    });
+}
 
-  function renderUserProfile(user) {
-    authList.classList.add('is-hidden');
-    userProfile = document.querySelector('.user-info');
-    userProfile.classList.remove('is-hidden');
-    userProfile.innerHTML = `
+function renderUserProfile(user) {
+  authList.classList.add('is-hidden');
+  userProfile = document.querySelector('.user-info');
+  userProfile.classList.remove('is-hidden');
+  userProfile.innerHTML = `
 	<button class="btn-signout">Sign out</button>
 	<p class="user-email">Email: ${auth.currentUser.email}</p>`;
-    signoutBtn = document.querySelector('.btn-signout');
-    signoutBtn.addEventListener('click', () => {
-      signOut(auth)
-        .then(() => {
-          console.log('Ты вышел🐷. А куда?🤓 А зачем?');
-          window.location.reload();
-        })
-        .catch(err => console.log(err));
-    });
-  }
-
+  signoutBtn = document.querySelector('.btn-signout');
+  signoutBtn.addEventListener('click', () => {
+    signOut(auth)
+      .then(() => {
+        console.log('Ты вышел🐷. А куда?🤓 А зачем?');
+        window.location.reload();
+      })
+      .catch(err => console.log(err));
+  });
+}
 
 // onAuthStateChanged(auth, user => {
 //   if (user) {
