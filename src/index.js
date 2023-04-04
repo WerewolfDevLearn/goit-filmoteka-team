5c293bac61678739d65b99ff529726d2d4aa2d1b
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-04-05 00:26:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import { showTrailer } from './js/services/trailer';
 import { refs, createFilmModal } from './js/services/modal-film';
 import './js/components/team-modal';
 import { ticker } from './js/components/search_running_line';
-
 import './js/services/login';
 
 import {
@@ -42,40 +41,6 @@ function stateModify(date) {
   STATE.movies = date.results;
   STATE.totalResults = date.total_results;
 }
-
-// const googleSignUp = document.querySelector('.registration-menu-singBtn');
-// const googleSignOut = document.querySelector('.registration-menu-logBtn');
-
-// googleSignUp.addEventListener('click', signUpWithGoogle);
-// googleSignOut.addEventListener('click', signOutFromGoogle);
-
-const email = 'were@gmail.com';
-const password = 'qwert2';
-
-// // signOut(email, password);
-// // userCreation(email, password);
-//
-userCreation;
-async function foo(email, password) {
-  const resp = await userCreation(email, password);
-  // console.log(STATE);
-  console.log(resp);
-  return resp;
-}
-foo(email, password);
-// async function getuzer(uid) {
-//   const gettedUser = await getUserData(uid);
-//   console.log('gettedUser: ', gettedUser);
-//   const user = {
-//     uid: gettedUser.uid,
-//     movies: gettedUser.movies,
-//   };
-//   console.log('user: ', user);
-//   // STATE.user = gettedUser;
-//   //   console.log(STATE);
-// }
-// const uid = '2Gyhw9T2zQaEJGbYLjPemy2zQOw2';
-// getuzer(uid);
 
 // -------------------------------------------------------------------------------------------- //
 // import { btnSwitcher } from './js/services/movie-operations';
