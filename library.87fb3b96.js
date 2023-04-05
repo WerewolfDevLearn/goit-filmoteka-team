bff51df59ab465fd95aeb1c473b167d841c4ad3e
WerewolfDevLearn <58573476+WerewolfDevLearn@users.noreply.github.com>
2023-04-05 15:48:35 +0000

    Deploying to gh-pages from @ WerewolfDevLearn/goit-filmoteka-team@3615c1264311bc6571e3c057d6ae95ea2066000d 🚀

--- library.87fb3b96.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(function(){var e=document.querySelector(".global-loader");null==e||e.remove()}));
-//# sourceMappingURL=library.87fb3b96.js.map
--- library.92d4cc04.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(function(){document.querySelector(".global-loader")?.remove()}));
-//# sourceMappingURL=library.92d4cc04.js.map
