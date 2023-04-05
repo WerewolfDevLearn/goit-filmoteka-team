874a5aeadcd842cfde49f7c118f4e30f9862c4ce
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-04-05 20:08:09 +0300

    fix gallerry

--- src/js/utiles/get_geners.js
+++ src/js/utiles/get_geners.js
@@ -1,11 +1,16 @@
-import {genreTable} from './geners.js';
+import { genreTable } from './geners.js';
 
 // Пошук потрібних жанрів по ID
 export function genresSearch(data) {
   const arrGenres = [];
-  const genre = data.map(element => {
+  const genre = data
+    .map(element => {
       return genreTable.filter(el => el.id === element).map(el => el.name);
-    }).join(', ');
-    arrGenres.push(genre)[0];
-    return arrGenres;
+    })
+    .join(', ');
+  arrGenres.push(genre)[0];
+  return arrGenres;
+}
+export function genresSearchBig(data) {
+  return data.map(el => el.name).join(', ');
 }
