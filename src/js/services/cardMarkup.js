0c3985cceda74db98aa5ae0626584db1692b5d40
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-04-11 10:33:21 +0300

    fix buttons in filmModal

--- src/js/services/cardMarkup.js
+++ src/js/services/cardMarkup.js
@@ -1,5 +1,13 @@
-export function cardMarkup({id, genreString, title, rating, releaseFullYear, posterPath, classStatus}){
-return `<li class="card__item" id="${id}">
+export function cardMarkup({
+  id,
+  genreString,
+  title,
+  rating,
+  releaseFullYear,
+  posterPath,
+  classStatus,
+}) {
+  return `<li class="card__item" id="${id}">
               <div class="card__container">
                 <img class="card__image" src="${posterPath}" alt="Зображення постера фільму ${title}" loading="lazy"/>
                 <div class="card__wrap">
@@ -11,5 +19,35 @@ return `<li class="card__item" id="${id}">
                 <p class="card__genres">${genreString} | ${releaseFullYear}</p>
                 <p class="card__rating">${rating}</p>
               </div>
-        </li>`
+        </li>`;
+}
+export function bigCardMarkup({
+  id,
+  genreString,
+  title,
+  rating,
+  vote_count,
+  releaseFullYear,
+  posterPath,
+  classStatus,
+  popularity,
+  original_title,
+  overview,
+}) {
+  return `<li class="card__item" id="${id}">
+              <div class="card__container">
+                <img class="card__image" src="${posterPath}" alt="Зображення постера фільму ${title}" loading="lazy"/>
+                <div class="card__wrap">
+                  <div class="card__activator ${classStatus}">
+                    <h2 class="card__name">${title}</h2>
+                  </div>
+                </div>
+                <div class="card__info">
+                <p class="card__rating">Vote / Votes ${rating} / ${vote_count}</p>
+                <p class="card__popularity">Popularity ${popularity}</p>
+                <p class="card__originaltitle">Original Title ${original_title}</p>
+                <p class="card__genres">Genre ${genreString}</p>
+                <p class="card__about">About ${overview}</p>
+              </div>
+        </li>`;
 }
--- src/js/services/createCardMarkup.js
+++ src/js/services/createCardMarkup.js
@@ -1,12 +1,12 @@
 import getPosterPath from '../utiles/get_poster_path';
-import { genresSearch } from '../utiles/get_geners';
+import { genresSearch, genresSearchBig } from '../utiles/get_geners';
 import { cardMarkup } from './cardMarkup';
 
 export function createCardMarkup(array) {
   // console.log('array: ', array);
   return array
-  .map(
-    ({ id, genre_ids, title, vote_average, release_date, poster_path }) => {
+    .map(
+      ({ id, genre_ids, title, vote_average, release_date, poster_path }) => {
         const stringLength = title.split('');
         const classStatus = quantityLetterOfString(stringLength);
         const posterPath = getPosterPath(poster_path);
@@ -20,15 +20,15 @@ export function createCardMarkup(array) {
           rating,
           releaseFullYear,
           posterPath,
-          classStatus
+          classStatus,
         });
       }
-      )
+    )
     .join('');
 }
 
-function quantityLetterOfString(stringLength){
-  if(stringLength.length <= 33){
+function quantityLetterOfString(stringLength) {
+  if (stringLength.length <= 33) {
     const classStatus = 'isNoActive';
     return classStatus;
   } else {
@@ -43,3 +43,52 @@ export function appendCardsMarkup(markup) {
 
 // Тимчасово
 const galleryEl = document.querySelector('.gallery');
+
+export function createbigCardMarkup(array) {
+  return array
+    .map(
+      ({
+        id,
+        genres,
+        title,
+        vote_average,
+        vote_count,
+        release_date,
+        popularity,
+        original_title,
+        overview,
+        poster_path,
+      }) => {
+        const stringLength = title.split('');
+        const classStatus = quantityLetterOfString(stringLength);
+        const posterPath = getPosterPath(poster_path);
+        const releaseFullYear = release_date.split('-')[0];
+        const rating = vote_average.toFixed(1);
+        const genreString = genresSearchBig(genres);
+        return cardMarkup({
+          id,
+          genreString,
+          title,
+          rating,
+          vote_count,
+          releaseFullYear,
+          posterPath,
+          classStatus,
+          popularity,
+          original_title,
+          overview,
+        });
+      }
+    )
+    .join('');
+}
+
+function quantityLetterOfString(stringLength) {
+  if (stringLength.length <= 33) {
+    const classStatus = 'isNoActive';
+    return classStatus;
+  } else {
+    const classStatus = 'isActive';
+    return classStatus;
+  }
+}
