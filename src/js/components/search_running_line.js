5c97e22591f5e73a71d7b87602acef54026d11c5
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-29 10:47:36 +0300

    add dane running line

--- src/js/components/search_running_line.js
+++ src/js/components/search_running_line.js
@@ -1,15 +1,24 @@
-const inputEl = document.querySelector(".header-search");
-const line = "Movie search";
-const speed = 200;
+const inputEl = document.querySelector('.header-search');
+const line = 'Movie search';
+const speed = 150;
+const breakTime = 7000;
 let i = 0;
 
- export function ticker() {
-  if(i < line.length){
-    i+=1;
-  inputEl.placeholder = line.substring(0,i);
-  } else {
-    inputEl.placeholder = " ";
-    i = 0;
+export function ticker() {
+  setTimeout(() => {
+    if (i < line.length) {
+      i += 1;
+      inputEl.placeholder = line.substring(0, i);
+      ticker();
+    } else {
+      setTimeout(tickerBreakTimeSetter, breakTime);
     }
-setTimeout(ticker, speed);
+    return;
+  }, speed);
+}
+
+export function tickerBreakTimeSetter() {
+  inputEl.placeholder = ' ';
+  i = 0;
+  ticker();
 }
